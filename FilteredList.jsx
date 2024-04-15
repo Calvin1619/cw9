05ed1268f4db02914f40d4068c1c97480aee400b
Calvin Kwok <104272282+Calvin1619@users.noreply.github.com>
2024-04-15 01:55:25 -0400

    Update FilteredList.jsx

--- FilteredList.jsx
+++ FilteredList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { DropdownButton, MenuItem } from  'react-bootstrap/Dropdown';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
 import List from './List';
 
 class FilteredList extends Component {
@@ -9,8 +9,13 @@ class FilteredList extends Component {
     //The state is just a list of key/value pairs (like a hashmap)
     //TODO (FilteredList): Add an additional state variable within this.state called "type" and set it to a default value
     this.state = {
-      search: ""
+      search: '', 
+      selectedType: 'all' 
     };
+  }
+
+  handleDropdownSelect = (type) => {
+    this.setState({ selectedType: type });
   }
 
   //Sets the state whenever the user types on the search bar
@@ -18,14 +23,17 @@ class FilteredList extends Component {
     this.setState({search: event.target.value.trim().toLowerCase()});
   }
 
-  //TODO (FilteredList): Set the state of the "type" state variable depending on what is passed in
-  onFilter = (event) => {
-
-  }
-
   //TODO (FilteredList): Change filterItem to take into account the "type" state variable when filtering
   filterItem = (item) => {
-      return item.name.toLowerCase().search(this.state.search) !== -1;
+    const { search, selectedType } = this.state;
+	
+    const itemName = item.name.toLowerCase();
+
+    const isMatched = itemName.includes(search);
+
+    const isTypeMatched = selectedType === 'all' || item.type === selectedType;
+
+    return isMatched && isTypeMatched;
   }
 
   render(){
@@ -35,11 +43,19 @@ class FilteredList extends Component {
           /*TODO (FilteredList): Create a Dropdown Menu with three different menu options: Fruit, Vegetables, and All*/
           <h1>Produce Search</h1>
 		  
+        <DropdownButton id="typeDropdown" title="Type Selection" onSelect={this.handleDropdownSelect}>
 
-		  
-          <input type = "text" placeholder = "Search" onChange = {this.onSearch} />
-          <List items = {this.props.items.filter(this.filterItem)} />
-        </div>
+          <Dropdown.Item eventKey="all">All</Dropdown.Item> <br />
+          <Dropdown.Item eventKey="Fruit">Fruit</Dropdown.Item> <br />
+          <Dropdown.Item eventKey="Vegetable">Vegetable</Dropdown.Item><br />
+        </DropdownButton>
+        <br />
+
+        <input type="text" placeholder="Search" onChange={this.onSearch} />
+        <br />
+
+        <List items={this.props.items.filter(this.filterItem)} />
+      </div>
     );
   }
 }
